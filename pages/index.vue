7d2f9234320349784253d443c45a8560e2ab374f
Chris Hong <chong@illfonic.com>
2021-11-22 10:51:39 -0800

    Update main index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,22 @@
 <template>
-  <Tutorial/>
+    <div>
+        <h2>Welcome to PolnareffLand!</h2>
+    </div>
 </template>
 
 <script>
-export default {}
+export default {
+    head() {
+        return {
+            title: 'Welcome to PolnareffLand',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'Pool of bad jokes'
+                }
+            ]
+        }
+    }
+}
 </script>
