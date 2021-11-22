9a5a9a65528a0540cbcb42e159089b0b33fbd4dc
Chris Hong <chong@illfonic.com>
2021-11-22 11:47:51 -0800

    Add single Joke component, pass it into pages/Jokes jokes array loop, Add a Jokes param folder, Update main README

--- pages/Jokes/index.vue
+++ pages/Jokes/index.vue
@@ -1,11 +1,19 @@
 <template>
-    <div>jokes</div>
+    <div>
+        <!-- Loop thru for each joke in the array using v-for directive -->
+        <!-- jokes array is in data() method -->
+        <Joke v-for="joke in jokes" :key="joke.id" :id="joke.id" :joke="joke.joke"/>
+    </div>
 </template>
 
 <script>
 import axios from 'axios';
+import Joke from '../../components/Joke'
 
 export default {
+    components: {
+        Joke
+    },
     // Returning object with data
     data() {
         return {
@@ -22,7 +30,7 @@ export default {
         try {
             const res = await axios.get('https://icanhazdadjoke.com/search', config);
             console.log(res.data);
-            this.jokes = res.data.results;
+            this.jokes = res.data.results; // stored in jokes array in data()
         } catch (err) {
             console.log(err);
         }
