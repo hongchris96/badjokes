8e7ce5d856a1670efa3031a9419fc683e4efb5fd
Chris Hong <chong@illfonic.com>
2021-11-22 11:31:28 -0800

    Update jokes index with vue lifecycle methods

--- pages/Jokes/index.vue
+++ pages/Jokes/index.vue
@@ -3,8 +3,42 @@
 </template>
 
 <script>
-export default {
+import axios from 'axios';
 
+export default {
+    // Returning object with data
+    data() {
+        return {
+            jokes: []
+        }
+    },
+    // For request to run as soon as components load, use lifecycle method created()
+    async created() {
+        const config = {
+            headers: {
+                'Accept': 'application/json'
+            }
+        }
+        try {
+            const res = await axios.get('https://icanhazdadjoke.com/search', config);
+            console.log(res.data);
+            this.jokes = res.data.results;
+        } catch (err) {
+            console.log(err);
+        }
+    },
+    head() {
+        return {
+            title: 'Bad Jokes',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'Pool of bad jokes'
+                }
+            ]
+        }
+    }
 }
 </script>
 
