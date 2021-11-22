22232df3a57d2c7385f0dcccb69529a6da1152bf
Chris Hong <chong@illfonic.com>
2021-11-22 10:40:56 -0800

    Create an about page

---
+++ pages/about.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1>Title of Bad Jokes</h1>
+        <p>This is an app from a nuxtjs tutorial</p>
+    </div>
+</template>
+
+<script>
+export default {
+    head() {
+        return {
+            title: 'About The App',
+            meta: [
+                {
+                    hid: 'uniqueIdentifier',
+                    name: 'actualMetaTag',
+                    content: 'Some kind of content here that shows up in the head meta tag'
+                },
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'Pool of bad jokes'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
